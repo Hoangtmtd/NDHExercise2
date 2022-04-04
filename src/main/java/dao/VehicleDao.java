ea8804d87d402ee29f8b484fc25d4109390e46c4
Hoangtmtd <hoangtmtd@gmail.com>
2022-04-04 08:48:17 +0700

    second commit

--- src/main/java/dao/VehicleDao.java
+++ src/main/java/dao/VehicleDao.java
@@ -7,6 +7,4 @@ import java.util.List;
 
 public interface VehicleDao extends BaseDao<Vehicle, Long> {
     List<Vehicle> findAllAvailableVehicle();
-    Vehicle getVehicleByRentalId(Long rentalId);
-    boolean updateRental(Long vehicleId);
 }
--- src/main/java/dao/impl/VehicleDaoImpl.java
+++ src/main/java/dao/impl/VehicleDaoImpl.java
@@ -21,37 +21,14 @@ public class VehicleDaoImpl extends BaseDaoImpl<Vehicle, Long> implements Vehicl
     @Override
     public List<Vehicle> findAllAvailableVehicle() {
         LOGGER.info("find all available vehicle dao");
-        try(Session session = HibernateUtils.getSession()){
+        try (Session session = HibernateUtils.getSession()) {
             Query<Vehicle> query = session.createQuery("FROM Vehicle WHERE rental_id = null");
             return query.getResultList();
-        } catch (PersistenceException e){
+        } catch (PersistenceException e) {
             LOGGER.error(e.getLocalizedMessage());
         }
         return null;
     }
 
-    @Override
-    public Vehicle getVehicleByRentalId(Long rentalId) {
-        try(Session session = HibernateUtils.getSession()){
-            Query<Vehicle> query = session.createQuery("FROM Vehicle WHERE rental_id = :rental");
-            query.setParameter("rental", rentalId);
-            return query.getSingleResult();
-        } catch (PersistenceException e){
-            LOGGER.error(e.getLocalizedMessage());
-        }
-        return null;
-    }
-
-    @Override
-    public boolean updateRental(Long vehicleId) {
-        try(Session session = HibernateUtils.getSession()){
-            Query<Vehicle> query = session.createQuery("UPDATE Vehicle SET rental_id = null WHERE id = :vehicleId");
-            query.setParameter("vehicleId", vehicleId);
-            return true;
-        } catch (PersistenceException e){
-            LOGGER.error(e.getLocalizedMessage());
-        }
-        return false;
-    }
 
 }
